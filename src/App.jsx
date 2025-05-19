210891c8495dd98dd183454972ca37520c2b2a0e
Shreyas Ladhe <shreyasladhe.23@gmail.com>
2025-05-20 01:33:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Navbar from './sections/Navbar'
+import Footer from './sections/Footer'
+import Contact from './sections/Contact'
+import Projects from './sections/Projects'
+import Experiences from './sections/Experiences'
+import Testimonial from './sections/Testimonial'
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+
+      <section id="home">
+        <Hero />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="work">
+        <Projects />
+      </section>
+
+      <section id="experiences">
+        <Experiences />
+      </section>
+
+      <section id="testimonial">
+        <Testimonial />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,191 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "PrepWise - AI Saas Interview Agent",
+    description:
+      "AI-powered interview preparation platform that customizes questions, conducts mock interviews, and delivers insightful feedback using speech and language models.",
+    subDescription: [
+      "Developed a full-stack web application using Next.js and Firebase for real-time authentication, storage, and database integration.",
+      "Integrated Gemini AI and Vapi AI to generate personalized interview questions, conduct mock calls, and provide AI-driven feedback.",
+      "Deployed the app on AWS Amplify with load balancer setup for production readiness.",
+    ],
+
+    href: "https://prepwise-seven.vercel.app",
+    logo: "/assets/projects/prepwise-logo.svg",
+    image: "/assets/projects/prepwise.png",
+    tags: [
+      {
+        id: 1,
+        name: "Vapi AI",
+        path: "/assets/logos/vapi.svg",
+      },
+      {
+        id: 2,
+        name: "AWS",
+        path: "/assets/logos/aws.svg",
+      },
+      {
+        id: 3,
+        name: "NextJs",
+        path: "/assets/logos/nextjs.svg",
+      },
+      {
+        id: 4,
+        name: "Genimi AI",
+        path: "/assets/logos/gemini-ai.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "StoreIt - A Secure Custom Storage Solution",
+    description:
+      "StoreIt is a secure, privacy-first file-sharing platform inspired by Google Drive, enabling users to upload, manage, and share files.",
+    subDescription: [
+      "Utilized Appwrite for backend services with OTP-based authentication and session management.",
+      "Integrated Amazon S3 for secure, temporary file storage using pre-signed URLs to protect data access.",
+      "Deployed on AWS using VPCs, Load Balancer, and Auto Scaling Groups for enterprise-grade scalability.",
+      "Set up CI/CD pipelines using Jenkins to maintain full control over build and deployment processes.",
+    ],
+    href: "https://storeit-topaz.vercel.app",
+    logo: "/assets/projects/storeit-logo.svg",
+    image: "/assets/projects/storeit.png",
+    tags: [
+      {
+        id: 1,
+        name: "AWS",
+        path: "/assets/logos/aws.svg",
+      },
+      {
+        id: 2,
+        name: "NextJS",
+        path: "/assets/logos/nextjs.svg",
+      },
+      {
+        id: 3,
+        name: "Appwrite",
+        path: "/assets/logos/appwrite.svg",
+      },
+      {
+        id: 4,
+        name: "Jenkins",
+        path: "/assets/logos/jenkins.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Many more coming soon..",
+    description:
+      "Coming Soon!",
+    subDescription: [
+      "Coming Soon!",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/coming-soon.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "",
+        path: "",
+      },
+      {
+        id: 2,
+        name: "",
+        path: "",
+      },
+      {
+        id: 3,
+        name: "",
+        path: "",
+      },
+      {
+        id: 4,
+        name: "",
+        path: "",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "GitHub",
+    href: "https://github.com/ShreyasLadhe",
+    icon: "/assets/socials/github.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/shreyasladhe",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Hashnode",
+    href: "https://clouddevopsfornewbies.hashnode.dev",
+    icon: "/assets/socials/hashnode.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "AWS Cloud Engineer",
+    job: "F13 Technologies (10+ months)",
+    date: "Aug 2024 - Present",
+    contents: [
+      "Led cloud infrastructure and application development for EmployeeOS and 2doo, driving deployment scalability and performance optimization.",
+      "Worked closely on indian government projects in collaboration with multiple Indian ministries, ensuring compliance with national security and data handling standards.",
+      "Spearheaded infrastructure modernization using AWS services, streamlining CI/CD pipelines and reducing deployment times by over 60%.",
+      "Mentored and managed fellow intern teams, conducting project evaluations, knowledge-sharing sessions, and aligning team efforts with sprint goals.",
+    ],
+  },
+  {
+    title: "Cloud Engineer Intern",
+    job: "ZeroToCloud (1+ months)",
+    date: "Apr 2025 - Present",
+    contents: [
+      "Designed and developed responsive websites for ZeroToCloud's cloud computing courses, ensuring performance, SEO optimization, and mobile compatibility.",
+      "Built scalable practice exam portals tailored for AWS certifications, with user authentication, progress tracking, and timed test features.",
+      "Created comprehensive question sets and detailed explanations aligned with AWS Cloud Practitioner exam blueprints.",
+      "Collaborated closely with the content and design teams to ensure seamless integration of technical content with frontend components.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Abhay Vashistha",
+    username: "Technical Lead, F13 Technologies",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://ca.slack-edge.com/T7BJUG8M8-UT6PQRY21-a3ada83a1198-512",
+  },
+  {
+    name: "Tarleen Kaur",
+    username: "Human Resource Specialist, F13 Technologies",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://ca.slack-edge.com/T7BJUG8M8-U02E2EXNHD2-47176f2550c8-512",
+  },
+  {
+    name: "Bhoomi Nawani",
+    username: "Human Resource Associate, F13 Technologies",
+    body: "Shreyas blends strong leadership with management, ensuring timely delivery and seamless teamwork.",
+    img: "https://ca.slack-edge.com/T7BJUG8M8-U0837G9SPNF-d8aef70bde0b-512",
+  },
+  {
+    name: "Amanpreet Singh",
+    username: "Founder, F13 Technologies",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://ca.slack-edge.com/T7BJUG8M8-U7BJUG8QJ-6f99a4b46f27-512",
+  },
+  {
+    name: "Snigdha Chaudhary",
+    username: "Technical Curriculum Developer, ZeroToCloud",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://media.licdn.com/dms/image/v2/D4D03AQG0Q8s94pb_zw/profile-displayphoto-shrink_400_400/B4DZQxtRKvG0Ag-/0/1736000738953?e=1753315200&v=beta&t=qQYxZWFfex6iHlGG0fOxNnKYqeqko9rRIt_uUWiU3FM",
+  },
+  {
+    name: "Lucy Wang",
+    username: "Founder, ZeroToCloud",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://media.licdn.com/dms/image/v2/C5603AQGKRbvieGxz1g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1629022924164?e=1753315200&v=beta&t=sqjjqPMgtpLXI6LVXqFeCCq6vf0jTnaejSVzK-7CkkQ",
+  },
+];
